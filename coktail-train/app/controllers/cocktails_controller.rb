b74ba3ce89a44e3a024c652ada1cfed8929e8cc7
Yoan Dorny <y.n.dorny@gmail.com>
2017-09-17 19:05:00 +0200

    photo uploader

--- coktail-train/app/controllers/cocktails_controller.rb
+++ coktail-train/app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:edit, :show, :destroy]
 
   def index
     @cocktails = Cocktail.all
@@ -8,22 +9,34 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def edit
+  end
+
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
       if @cocktail.save
-        redirect_to cocktail_path(@cocktail)
-      else
-        render :new
-      end
+    redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
 private
-  def cocktail_params
-    params.require(:cocktail).permit(:name)
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+    def cocktail_params
+      params.require(:cocktail).permit(:name, :photo, :photo_cache)
+    end
+
 end
